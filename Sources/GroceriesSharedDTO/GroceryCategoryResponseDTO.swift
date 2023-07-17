9db5a0fd323456aae497ff89801ba580305d359b
Computer Athlete <admin@computerathlete.dev>
2023-07-17 16:41:24 +1000

    Lesson 97 (children) commit

--- Sources/GroceriesSharedDTO/GroceryCategoryResponseDTO.swift
+++ Sources/GroceriesSharedDTO/GroceryCategoryResponseDTO.swift
@@ -16,10 +16,12 @@ public struct GroceryCategoryResponseDTO: Codable {
     public let id: UUID
     public let title: String
     public let colourCode: String
+    public let items: [GroceryItemResponseDTO]
     
-    public init(id: UUID, title: String, colourCode: String) {
+    public init(id: UUID, title: String, colourCode: String, items: [GroceryItemResponseDTO]) {
         self.id = id
         self.title = title
         self.colourCode = colourCode
+        self.items = items
     }
 }
